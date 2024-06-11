a252ccdf85ad0b0574453ec5063f020a6c2fdd2e
Aditya Dutta <adi22dutta@gmail.com>
2024-06-12 03:34:12 +0530

    modified so that /home/user is ~

--- shell_utils.c
+++ shell_utils.c
@@ -1,12 +1,25 @@
+char* cwd_home_tilde(char *cwd) {
+    char *cwd_orig = cwd;
+    char *home = getenv("HOME");
+    while (*home != '\0') {
+        if (*home++ != *cwd++) {
+            return cwd_orig;
+        }
+    }
+    char* cwd_final = (char *)malloc((MAX_PATH_LEN + 1) * sizeof(char));
+    strcat(cwd_final, cwd);
+    free(cwd_orig);
+    return cwd_final;
+}
+
 void print_prompt(){
-    char cwd[MAX_PATH_LEN];
+    char* cwd = (char *)malloc((MAX_PATH_LEN + 1) * sizeof(char));
     getcwd(cwd, MAX_PATH_LEN);
     char *username = getenv("USER");
     char hostname[HOST_NAME_MAX + 1];
     gethostname(hostname, sizeof(hostname));
     
-    char *home = getenv("HOME");
-    if (strcmp(cwd, home) == 0) strcpy(cwd, "~");
+    cwd = cwd_home_tilde(cwd);
 
     printf("\e[1m%c[%dm%s@%s\e[m", 0x1B, 32, username, hostname);
     printf("\e[1m%c[%dm:\e[m", 0x1B, 0);
